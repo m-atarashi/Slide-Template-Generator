413c183fc70668c9b1fda9721c12cc6775af7267
matarashi <g2123002@fun.ac.jp>
2023-06-06 23:19:58 +0900

    Move a viewport Meta Element in _document to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
-import '@/index.css'
+import 'index.css'
 
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,6 @@ export default function Document() {
             <Head>
                 <meta charSet="utf-8" />
                 <meta title="Slide Template Generator for Papers" />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <meta name="mobile-web-app-capable" content="yes" />
                 <meta name="apple-mobile-web-app-capable" content="yes" />
                 <link rel="icon" href="" />
